292d3fe796e7a1db5b8deb521e5e1950667868b7
Carla Serrano <carlacarly8@gmail.com>
2022-01-17 19:09:47 -0300

    Reto-8

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
 const cosasQueAprendimos = [
   {
-    tema: 'terminal',
-    class: '',
+    tema: "terminal",
+    class: "",
   },
   {
-    tema: 'node',
-    class: '',
+    tema: "node",
+    class: "",
   },
   {
-    tema: 'oop',
-    class: '',
+    tema: "oop",
+    class: "",
   },
   {
-    tema: 'flexbox',
-    class: '',
+    tema: "flexbox",
+    class: "",
   },
   {
-    tema: 'css',
-    class: '',
+    tema: "css",
+    class: "",
   },
   {
-    tema: 'dom',
-    class: 'special',
+    tema: "dom",
+    class: "special",
   },
 ];
 
-function main() {}
+function main() {
+  // 1 Recorrer el array
+  cosasQueAprendimos.forEach((cosa) => {
+    // 2 Creaar el li (hijo)
+    const nuevoItem = document.createElement("li");
+    nuevoItem.innerText = cosa.tema;
+    nuevoItem.className = cosa.class;
+    // 3 Agregar el li al padre (ul)
+    document.querySelector("ul.lista").appendChild(nuevoItem);
+  });
+}
 
 main();
